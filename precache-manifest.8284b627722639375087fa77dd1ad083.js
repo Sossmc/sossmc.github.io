8f8975fca260bd7e0c3081a0e76c90dbc1ed0ea8
sossmc <sossmc@outlook.com>
2019-04-08 14:44:15 +0800

    update ico

---
+++ precache-manifest.8284b627722639375087fa77dd1ad083.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "08b0336f140b39c08bc7",
+    "url": "./static/css/main.0ef7b988.chunk.css"
+  },
+  {
+    "revision": "08b0336f140b39c08bc7",
+    "url": "./static/js/main.08b0336f.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "6e96b77b8012de2696a9",
+    "url": "./static/js/2.6e96b77b.chunk.js"
+  },
+  {
+    "revision": "66cbb621b431bf32041a5c478e5539c0",
+    "url": "./static/media/feather.66cbb621.woff"
+  },
+  {
+    "revision": "a940fe89dbfe9d1d89fc1aa0488fe032",
+    "url": "./static/media/feather.a940fe89.ttf"
+  },
+  {
+    "revision": "5fad700adc948cb51404d55833347f51",
+    "url": "./static/media/feather.5fad700a.eot"
+  },
+  {
+    "revision": "023ba0824a99109c4a63622228dd6354",
+    "url": "./static/media/feather.023ba082.svg"
+  },
+  {
+    "revision": "e965f5cd8974112ce6cbd51b902ced18",
+    "url": "./index.html"
+  }
+];
